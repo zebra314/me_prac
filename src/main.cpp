6b1020752008359acee67e2dff5a2fadfdf867b9
zebra314 <kidalfonso@gmail.com>
2024-03-23 01:51:12 +0800

    Add plate control test

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,11 @@
 #include <Plotter.h>
 #include "wheel.h"
 #include "arm.h"
+#include "plate.h"
 
 // #define ENABLE_PLOTTER
 
-/* PINs */
-const byte ENCA = 2;
-const byte ENCB = 4;
-const byte IN3 = 7;
-const byte IN4 = 6;
-const byte SPD = 5;
-
-/* ROBOT COMPONENTs */
-Wheel wheel;
-Arm arm;
-
-/* USER INPUT */
-int inputValue = 0;
-int buffer;
-
-/* FUNCTIONS */
-void get_serial_input();
+Plate plate;
 
 /* PLOT */
 #ifdef ENABLE_PLOTTER
@@ -32,9 +17,8 @@ double plot_x;
 
 void setup() {
   Serial.begin(9600);
-  wheel.wheel_connect(IN4, IN3, SPD, ENCA, ENCB);
-  // arm.arm_connect(8,9,10,11,12,13);
-
+  plate.plate_connect();
+  
   #ifdef ENABLE_PLOTTER
   plotter.Begin();
   plotter.AddTimeGraph("Motor position", 5000, "time", plot_x);
@@ -42,28 +26,12 @@ void setup() {
 }
 
 void loop() {
-  // int target = -2000;
-  // int pwm = wheel.pid_control(target);
-  // wheel.wheel_move(pwm);
+  plate.plate_command(Command::LINEAR_POSI, -2000);
   
-  Serial.println(wheel.get_encoder_count());
+  // Serial.println(plate.plate_check_enc(WheelType::FR));
 
   #ifdef ENABLE_PLOTTER
-  plot_x = wheel.get_encoder_count()* -1;
+  plot_x = plate.plate_check_enc(WheelType::FR) * -1;
   plotter.Plot();
   #endif
-}
-
-void get_serial_input() {
-  char tmp;
-  if (Serial.available() > 0) {
-    tmp = Serial.read();
-    if (tmp == '\n') {
-      Serial.println(String(buffer));
-      inputValue = buffer;
-      buffer = 0;
-    } else if (tmp >= '0' && tmp <= '9') {
-      buffer = buffer * 10 + (tmp - '0');
-    }
-  }
 }
