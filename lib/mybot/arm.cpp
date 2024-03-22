b92cb1fc0fcffb5145bf0b074ea8e32dfadfac4c
zebra314 <kidalfonso@gmail.com>
2024-03-23 01:50:30 +0800

    Update pin names

--- lib/mybot/arm.cpp
+++ lib/mybot/arm.cpp
@@ -8,26 +8,29 @@ Arm::~Arm() {
   // Destructor
 }
 
-void Arm::arm_connect(byte servo_plate_pin, byte servo_2_pin, byte servo_3_pin, byte servo_4_pin, byte servo_5_pin, byte servo_gripper_pin) {
-  this->servo_plate_pin = servo_plate_pin;
+void Arm::arm_connect(byte servo_0_pin, byte servo_1_pin, byte servo_2_pin, byte servo_3_pin, byte servo_4_pin, byte servo_gripper_pin) {
+  this->servo_0_pin = servo_0_pin;
+  this->servo_1_pin = servo_1_pin;
   this->servo_2_pin = servo_2_pin;
   this->servo_3_pin = servo_3_pin;
   this->servo_4_pin = servo_4_pin;
-  this->servo_5_pin = servo_5_pin;
   this->servo_gripper_pin = servo_gripper_pin;
 
-  this->servo_plate.attach(servo_plate_pin);
+  this->servo_0.attach(servo_0_pin);
+  this->servo_1.attach(servo_1_pin);
   this->servo_2.attach(servo_2_pin);
   this->servo_3.attach(servo_3_pin);
   this->servo_4.attach(servo_4_pin);
-  this->servo_5.attach(servo_5_pin);
   this->servo_gripper.attach(servo_gripper_pin);
 }
 
 void Arm::arm_iso_ctrl(int plate_num, int deg){
   switch (plate_num) {
+    case 0:
+      this->servo_0.write(deg);
+      break;
     case 1:
-      this->servo_plate.write(deg);
+      this->servo_1.write(deg);
       break;
     case 2:
       this->servo_2.write(deg);
@@ -37,9 +40,6 @@ void Arm::arm_iso_ctrl(int plate_num, int deg){
       break;
     case 4:
       this->servo_4.write(deg);
-      break;
-    case 5:
-      this->servo_5.write(deg);
       break;
     case 6:
       this->servo_gripper.write(deg);
--- lib/mybot/arm.h
+++ lib/mybot/arm.h
@@ -6,25 +6,25 @@
 
 class Arm {
 private:
-  byte servo_plate_pin;
+  byte servo_0_pin;
+  byte servo_1_pin;
   byte servo_2_pin;
   byte servo_3_pin;
   byte servo_4_pin;
-  byte servo_5_pin;
   byte servo_gripper_pin;
 
-  Servo servo_plate;
+  Servo servo_0;
+  Servo servo_1;
   Servo servo_2;
   Servo servo_3;
   Servo servo_4;
-  Servo servo_5;
   Servo servo_gripper;
 
 public:
   Arm();
   ~Arm();
 
-  void arm_connect(byte servo_plate_pin, byte servo_2_pin, byte servo_3_pin, byte servo_4_pin, byte servo_5_pin, byte servo_gripper_pin);
+  void arm_connect(byte servo_0_pin, byte servo_1_pin, byte servo_2_pin, byte servo_3_pin, byte servo_4_pin, byte servo_gripper_pin);
   void arm_iso_ctrl(int plate_num, int deg);
 };
 
