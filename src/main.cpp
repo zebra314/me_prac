861eee4895a210bc7ba23f9e701339ab7133e61a
zebra314 <kidalfonso@gmail.com>
2024-03-30 07:19:40 +0800

    Remove BT debug msgs

--- src/main.cpp
+++ src/main.cpp
@@ -2,54 +2,47 @@
 #include "plate.h"
 #include "blueTooth.cpp"
 
-// these are bluetooth related code
+// BT param
 char rcv = 0x04;
-Command cmd;
 
+Command cmd;
 Plate plate(DEBUG::NONE);
 
 void setup() {
-  Serial.begin(9600);
   plate.plate_connect();
   BlueTooth_setup();
-  Serial.println("Setup done");
 }
 
 void loop() {
-  // read the bluetooth signal and translate it to command
+  // Read the bluetooth signal and translate it to command
   if(BlueTooth_read(&rcv)){
     switch (rcv)
     {
     case '0':
-      Serial.println("FORWARD");
       plate.plate_command(Command::LINEAR_PWM, 120);
       break;
 
     case '1':
-      Serial.println("BACKWARD");
       plate.plate_command(Command::LINEAR_PWM, -120);
       break;
 
     case '2':
-      Serial.println("CLOCKWISE");
       plate.plate_command(Command::ANGULAR_PWM, 120);
       break;
 
     case '3':
-      Serial.println("COUNTER_CLOCKWISE");
       plate.plate_command(Command::ANGULAR_PWM, -120);
       break;
     
     case '4':
-      Serial.println("STOP");
       plate.plate_command(Command::PAUSE, 0);
       break;
 
     default:
-      Serial.println(rcv);
       break;
     }
   }
   
+  // plate.plate_command(Command::LINEAR_POSI, 3);
   // plate.plate_command(Command::DEBUG, 0);
 }
