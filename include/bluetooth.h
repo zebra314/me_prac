32272b174d878fceed70fba95904c639e09ccf62
dragonwu0919 <dragonwu0919@gmail.com>
2024-05-08 09:36:22 +0800

    Define the Bluetooth cmd
    Create a new .h file, and in there define the BT cmd in a ENUM type.

---
+++ include/bluetooth.h
@@ -0,0 +1,17 @@
+typedef enum bluetooth
+{
+    CAR_CLOCKWISE = '2',
+    CAR_COUNTERCLOCKWISE = '3',
+    CAR_FORWARD = '0',
+    CAR_BACKWORD = '1',
+    CAR_STOP = '4',
+    ARM_M1_C = 'A',
+    ARM_M1_CV = 'a',
+    ARM_M2_C = 'B',
+    ARM_M2_CV = 'b',
+    ARM_M3_C = 'C',
+    ARM_M3_CV = 'c',
+    ARM_HALT = 'e',
+    ARM_FASTER = 'D',
+    ARM_SLOWER = 'd',
+} bluetooth_t;
